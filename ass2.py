7cd20610988a28e42054a09dd61b544cabd541b1
omjadas <omja.das@gmail.com>
2018-05-05 19:43:33 +1000

    added main sentinel and preprocess function

--- ass2.py
+++ ass2.py
@@ -1,2 +1,16 @@
 import sklearn
 import numpy as np
+import csv
+
+FILENAME = ""
+
+
+def preprocess(file_to_open):
+    with open(file_to_open, "r") as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return None
+
+
+if __name__ == "__main__":
+    preprocess(FILENAME)
