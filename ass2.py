d2808db6d76fb1d35609e4940552713286b411ef
omjadas <omja.das@gmail.com>
2018-05-05 21:45:16 +1000

    added pandas to project

--- ass2.py
+++ ass2.py
@@ -1,14 +1,18 @@
 import sklearn
 import numpy as np
+import pandas as pd
 import csv
 
 FILENAME = ""
 
 
 def preprocess(file_to_open):
+    training_data = pandas.read_csv(FILENAME)
     with open(file_to_open, "r") as f:
         reader = csv.reader(f)
         data = list(reader)
+
+    print(training_data);
     return None
 
 
