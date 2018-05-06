acd85252bf144d0dd1a1e22685e3fc2fa1ac93ba
omjadas <omja.das@gmail.com>
2018-05-06 20:36:22 +1000

    changed preprocess to main and added preprocess, added vectoriser

--- ass2.py
+++ ass2.py
@@ -1,16 +1,37 @@
-import sklearn
 import numpy as np
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-FILENAME = "dev_raw.csv"
+TRAIN = "train_raw.csv"
+DEV = "dev_raw.csv"
+TEST = "test_raw.csv"
 
 
-def preprocess(file_to_open):
-    training_data = pd.read_csv(FILENAME, header=None)
+def main():
+    training_data = preprocess(TRAIN)
+    dev_data = preprocess(DEV)
+    test_data = preprocess(TEST)
 
-    print(training_data);
+    train(training_data, dev_data, test_data)
+    return None
+
+def preprocess(file_path):
+    data = pd.read_csv(TRAIN, header=None)
+    data = data[data.iloc[:, -1] != '?']
+    data = data[[2, 6]]
+    return data
+
+def train(training_data, dev_data, test_data):
+
+    vectoriser = TfidfVectorizer(stop_words='english')
+    train_vector = vectoriser.fit_transform(training_data.iloc[:,1])
+    # test_vector = vectoriser.transform(X_test)
+
+    print(train_vector)
+
+    # clf.fit(training_data.iloc[:, -1], new_data)
     return None
 
 
 if __name__ == "__main__":
-    preprocess(FILENAME)
+    main()
