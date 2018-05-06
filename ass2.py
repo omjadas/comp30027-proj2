e1e48102c506c07b67d62684dcfdd88cf9a4e91a
omjadas <omja.das@gmail.com>
2018-05-06 21:08:14 +1000

    added predict function prototype

--- ass2.py
+++ ass2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 TRAIN = "train_raw.csv"
 DEV = "dev_raw.csv"
@@ -15,24 +16,30 @@ def main():
     train(training_data, dev_data, test_data)
     return None
 
+
 def preprocess(file_path):
     data = pd.read_csv(TRAIN, header=None)
     data = data[data.iloc[:, -1] != '?']
     data = data[[2, 6]]
     return data
 
+
 def train(training_data, dev_data, test_data):
 
     vectoriser = TfidfVectorizer(stop_words='english')
-    train_vector = vectoriser.fit_transform(training_data.iloc[:,1])
-    dev_vector = vectoriser.transform(dev_data.iloc[:,1])
+    train_vector = vectoriser.fit_transform(training_data.iloc[:, 1])
+    dev_vector = vectoriser.transform(dev_data.iloc[:, 1])
     # test_vector = vectoriser.transform(test_data.iloc[:,1])
 
-    print(train_vector)
-    print(dev_vector)
+    print(train_vector.shape)
+    print(dev_vector.shape)
 
     # clf.fit(training_data.iloc[:, -1], new_data)
     return None
+
+
+def predict():
+    pass
 
 
 if __name__ == "__main__":
